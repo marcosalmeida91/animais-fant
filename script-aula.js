b92c0714e117f1e85f19ca5105cf6e464e3f62f2
Marcos <marcos.fadsilva@gmail.com>
2021-02-17 18:41:55 -0300

    forEach e arrow function

--- script-aula.js
+++ script-aula.js
@@ -1,22 +1,9 @@
-//retornar todas as imagens  do site
-const imagens = document.querySelectorAll('img');
-console.log(imagens);
+// retornar cada paragrafo do site:
+const paragrafos = document.querySelectorAll('p');
+console.log(paragrafos);
 
+paragrafos.forEach((item) => console.log(item));
 
-//retornar apenas as imagens  que começam com  a palavra imagem
-const imagens2 = document.querySelectorAll('img[src^="img/imagem"]');
-console.log(imagens2);
-
-//selecionar todos os links internos(onde o href começa com #. O cód ^= irá selecionar apenas os que começam com #) 
-const apenasLinks = document.querySelectorAll('[href^="#"]');
-console.log(apenasLinks);
-
-//selecionar o primeiro h2 dentro de .animais-descricao
-const primeiroH2 = document.querySelector('.animais-descricao h2');
-console.log(primeiroH2);
-
-//selecionar o último p do site
-const ultimoP = document.querySelectorAll('p');
-//console.log(ultimoP[24]);  pode usar o .length para contalibizar a quantidade de P ou usar assim:
-console.log(ultimoP[ultimoP.length - 1]);
+//mostrar o texto dos paragrafos:(o .innerText serve para mostrar apenas o texto das tags selecionadas)
+paragrafos.forEach((item) => console.log(item.innerText));
 
--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-const animais = document.getElementById('animais');
-console.log(animais);
+// o index serve para identificar a posiçao do item dentro da array
+const img = document.querySelectorAll('img');
+img.forEach(function(item, index, array) {
+  //console.log(item, index, array);
+});
 
-// vai selecionar todos os itens que tiver a classe em questão, chamada de NodeList
-const gridSectionNode = document.querySelectorAll('.grid-section');
+// o array.from serve para transformar um objeto em array.
+const titulos = document.getElementsByClassName('titulo');
+const titulosArray = Array.from(titulos);
 
-// seleciona todas as classes em questão e retorna uma HTML Collection, possui menos metodos que o NodeList
-const gridSectionHTML = document.getElementsByClassName('grid-section');
+titulosArray.forEach(function(item){
+  //console.log(item);
+});
 
-console.log(gridSectionNode);
+//arrow function serve para encurtar a expressao da função
+const imgs = document.querySelectorAll('img');
+imgs.forEach((item, index) => {
+  //console.log(item, index);
+});
 
-console.log(gridSectionHTML);
+// se tiver apenas 1 item, não precisa usar parentêses.
+const imgs2 = document.querySelectorAll('img');
+imgs.forEach(item => {
+  //console.log(item);
+});
+
+// se não possuir argumentos, deve colocar parentêses
+// let i = 0;
+// const imgs3 = document.querySelectorAll('img');
+// imgs.forEach(() => {
+//   console.log(i++);
+// });
+
+// quando tiver uma linha de código também funciona dessa maneira:
+let i = 0;
+imgs.forEach(() => console.log(i++));
